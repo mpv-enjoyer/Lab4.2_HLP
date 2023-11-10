5fc62d48890900a9e5c93a0fc9e43dda97510ebe
mpv-enjoyer <Disblocker.just@gmail.com>
2023-11-10 18:03:56 +0300

    First commit

---
+++ CustomIntQueue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class QueueElement
+{ 
+    int CurrentElement = 0;
+    QueueElement? NextElement = null;
+    public QueueElement(int element = 0)
+    {
+        CurrentElement = element;
+    }
+    public int Get(int index)
+    {
+        if (index == 0) return CurrentElement;
+        if (NextElement == null) throw new Exception("'get' out of bounds");
+        return NextElement.Get(index - 1);
+    }
+    public void Set(int index, int value)
+    {
+        if (index == 0)
+        {
+            CurrentElement = value;
+            return;
+        }
+        if (NextElement == null) throw new Exception("'set' out of bounds");
+        NextElement.Set(index - 1, value);
+    }
+    public bool MoveBack()
+    {
+        if (NextElement == null) return true;
+        CurrentElement = NextElement.Get(0);
+        if (NextElement.MoveBack())
+        {
+            NextElement = null;
+        }
+        return false;
+    }
+    public void RemoveAt(int index)
+    {
+        if (index != 0)
+        {
+            if (NextElement == null) throw new Exception("'removeat' out of bounds");
+            NextElement.RemoveAt(index - 1);
+            return;
+        }
+        if (MoveBack()) NextElement = null;
+    }
+    public void Generate(int size)
+    {
+        if (size <= 1) return;
+        NextElement = new QueueElement();
+        NextElement.Generate(size - 1);
+    }
+    public int SizeCalc()
+    {
+        if (NextElement == null) return 1;
+        return NextElement.SizeCalc() + 1;
+    }
+    public void Expand(int value)
+    {
+        if (NextElement == null)
+        {
+            NextElement = new QueueElement(value);
+            return;
+        }
+        NextElement.Expand(value);
+    }
+    public void ClearNext()
+    {
+        NextElement = null;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿QueueElement queue = new();
+string? input = " ";
+Console.WriteLine("push pop front size clear exit");
+while (true)
+{
+    Console.Write("> ");
+    input = Console.ReadLine();
+    if (input == null) continue;
+    string[] split = input.Split();
+    string command = split[0];
+    int argument = split.Length > 1 ? int.Parse(split[1]) : 0;
+    switch (command)
+    {
+        case "push":
+            Push(queue, argument);
+            Console.WriteLine("ok");
+            break;
+        case "pop":
+            Console.WriteLine(Pop(queue));
+            break;
+        case "front":
+            Console.WriteLine(Front(queue));
+            break;
+        case "size":
+            Console.WriteLine(Size(queue));
+            break;
+        case "clear":
+            Clear(queue);
+            Console.WriteLine("ok");
+            break;
+        case "exit":
+            Console.WriteLine("bye");
+            return;
+    }
+}
+
+//1st element is unaccessible
+void Push(QueueElement entity, int value)
+{
+    entity.Expand(value);
+}
+int Size(QueueElement entity)
+{
+    return entity.SizeCalc() - 1;
+}
+int Front(QueueElement entity)
+{
+    if (Size(entity) == 0) throw new Exception("front from null");
+    return entity.Get(1);
+}
+int Pop(QueueElement entity)
+{
+    if (Size(entity) == 0) throw new Exception("pop from null");
+    int value = Front(entity);
+    entity.RemoveAt(0);
+    return value;
+}
+void Clear(QueueElement entity)
+{
+    entity.ClearNext();
+}
